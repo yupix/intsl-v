595f9acf96dad15d4ef13af7180b8c169f2c7898
yupix <yupi0982@outlook.jp>
2020-08-27 03:32:05 +0900

    refactor: spigotserver.sh

--- usr/lib/intsl_v/spigotserver.sh
+++ usr/lib/intsl_v/spigotserver.sh
@@ -6,143 +6,31 @@ while :; do
         echo "入力したバージョン: $INPUT_SERVER_VERSION"
     fi
     case $INPUT_SERVER_VERSION in
-    1.8)
+    1.8 | 1.8.1 | 1.8.2 | 1.8.3 | 1.8.4 | 1.8.5 | 1.8.6 | 1.8.7 | 1.8.8 | 1.8.9)
         buildversion="$INPUT_SERVER_VERSION"
         break
         ;;
-    1.8.1)
+    1.9 | 1.9.1 | 1.9.2 | 1.9.3 | 1.9.4)
         buildversion="$INPUT_SERVER_VERSION"
         break
         ;;
-    1.8.2)
+    1.10 | 1.10.1 | 1.10.2 | 1.11 | 1.11.1 | 1.11.2)
         buildversion="$INPUT_SERVER_VERSION"
         break
         ;;
-    1.8.3)
+    1.12 | 1.12.1 | 1.12.2)
         buildversion="$INPUT_SERVER_VERSION"
         break
         ;;
-    1.8.4)
+    1.13 | 1.13.1 | 1.13.2)
         buildversion="$INPUT_SERVER_VERSION"
         break
         ;;
-    1.8.5)
+    1.14 | 1.14.1 | 1.14.2 | 1.14.3 | 1.14.4)
         buildversion="$INPUT_SERVER_VERSION"
         break
         ;;
-    1.8.6)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.8.7)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.8.8)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.8.9)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.9)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.9.1)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.9.2)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.9.3)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.9.4)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.10)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.10.1)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.10.2)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.11)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.11.1)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.11.2)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.12)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.12.1)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.12.2)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.13)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.13.1)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.13.2)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.14)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.14.1)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.14.2)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.14.3)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.14.4)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.15)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.15.1)
-        buildversion="$INPUT_SERVER_VERSION"
-        break
-        ;;
-    1.15.2)
+    1.15 | 1.15.1 | 1.15.2)
         buildversion="$INPUT_SERVER_VERSION"
         break
         ;;
