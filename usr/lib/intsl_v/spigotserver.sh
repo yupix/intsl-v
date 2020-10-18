b10224118c6c9e1d8f39db4a55ac3230a01abb1f
yupix <yupi0982@outlook.jp>
2020-10-18 21:42:33 +0900

    feat: support 1.16.3(spigot edition)

--- usr/lib/intsl_v/spigotserver.sh
+++ usr/lib/intsl_v/spigotserver.sh
@@ -6,45 +6,45 @@ while :; do
         echo "入力したバージョン: $INPUT_SERVER_VERSION"
     fi
     case $INPUT_SERVER_VERSION in
-        1.8 | 1.8.1 | 1.8.2 | 1.8.3 | 1.8.4 | 1.8.5 | 1.8.6 | 1.8.7 | 1.8.8 | 1.8.9)
-            buildversion="$INPUT_SERVER_VERSION"
-            break
+    1.8 | 1.8.1 | 1.8.2 | 1.8.3 | 1.8.4 | 1.8.5 | 1.8.6 | 1.8.7 | 1.8.8 | 1.8.9)
+        buildversion="$INPUT_SERVER_VERSION"
+        break
         ;;
-        1.9 | 1.9.1 | 1.9.2 | 1.9.3 | 1.9.4)
-            buildversion="$INPUT_SERVER_VERSION"
-            break
+    1.9 | 1.9.1 | 1.9.2 | 1.9.3 | 1.9.4)
+        buildversion="$INPUT_SERVER_VERSION"
+        break
         ;;
-        1.10 | 1.10.1 | 1.10.2)
-            buildversion="$INPUT_SERVER_VERSION"
-            break
+    1.10 | 1.10.1 | 1.10.2)
+        buildversion="$INPUT_SERVER_VERSION"
+        break
         ;;
-        1.11 | 1.11.1 | 1.11.2)
-            buildversion="$INPUT_SERVER_VERSION"
-            break
+    1.11 | 1.11.1 | 1.11.2)
+        buildversion="$INPUT_SERVER_VERSION"
+        break
         ;;
-        1.12 | 1.12.1 | 1.12.2)
-            buildversion="$INPUT_SERVER_VERSION"
-            break
+    1.12 | 1.12.1 | 1.12.2)
+        buildversion="$INPUT_SERVER_VERSION"
+        break
         ;;
-        1.13 | 1.13.1 | 1.13.2)
-            buildversion="$INPUT_SERVER_VERSION"
-            break
+    1.13 | 1.13.1 | 1.13.2)
+        buildversion="$INPUT_SERVER_VERSION"
+        break
         ;;
-        1.14 | 1.14.1 | 1.14.2 | 1.14.3 | 1.14.4 | 1.14.5)
-            buildversion="$INPUT_SERVER_VERSION"
-            break
+    1.14 | 1.14.1 | 1.14.2 | 1.14.3 | 1.14.4 | 1.14.5)
+        buildversion="$INPUT_SERVER_VERSION"
+        break
         ;;
-        1.15 | 1.15.1 | 1.15.2)
-            buildversion="$INPUT_SERVER_VERSION"
-            break
+    1.15 | 1.15.1 | 1.15.2)
+        buildversion="$INPUT_SERVER_VERSION"
+        break
         ;;
-        1.16 | 1.16.1 | 1.16.2)
-            buildversion="$INPUT_SERVER_VERSION"
-            break
+    1.16 | 1.16.1 | 1.16.2 | 1.16.3)
+        buildversion="$INPUT_SERVER_VERSION"
+        break
         ;;
-        *)
-            echo "存在しないバージョンです"
-            read -r -p ">" INPUT_SERVER_VERSION
+    *)
+        echo "存在しないバージョンです"
+        read -r -p ">" INPUT_SERVER_VERSION
         ;;
     esac
 done
